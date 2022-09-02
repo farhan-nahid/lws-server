2f4ec0eea3af5651006109f6f6894150b8d2ad60
Farhan Ahmed Nahid <74184886+farhan-nahid@users.noreply.github.com>
2022-09-02 09:35:54 +0600

    Add files via upload

---
+++ db.js
@@ -0,0 +1,9 @@
+const todos = require("./data/todos");
+const videos = require("./data/db_videos");
+const transactions = require("./data/transactions");
+
+module.exports = {
+  todos,
+  videos,
+  transactions,
+};
