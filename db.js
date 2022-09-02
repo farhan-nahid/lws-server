7a10c122deb75389ed55a33afcf5241624e7ab74
Farhan Ahmed Nahid <74184886+farhan-nahid@users.noreply.github.com>
2022-09-02 10:28:44 +0600

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const todos = require("./data/todos");
+const tags = require("./data/tags");
 const videos = require("./data/db_videos");
 const transactions = require("./data/transactions");
 
 module.exports = {
   todos,
   videos,
+  tags,
   transactions,
 };
